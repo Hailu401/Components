3f6892e0770ecbf8b4592dace2b5781723d5b58f
Hailu401 <123373063+Hailu401@users.noreply.github.com>
2024-09-19 14:08:00 -0400

    add comment

--- Login.jsx
+++ Login.jsx
@@ -9,7 +9,7 @@ function Login() {
     const [ email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
+//function foor sign route
 
     const signIn = (e) => {
         e.preventDefault();
